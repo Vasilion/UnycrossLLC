e40d89fe49f134664cf0f17b9b3e208b5d279ed1
Vasilion <lukevasilion@gmail.com>
2024-03-27 14:02:49 -0400

    fix email form

--- email.php
+++ email.php
@@ -1,25 +1,22 @@
 <?php
-$emailTo = 'unycross@gmail.com';  //Recipient's E-mail
+$emailTo = 'unycross@gmail.com';  // Recipient's E-mail
 $subject = $_REQUEST['subject'];
-$name =  $_REQUEST['name'];
-$message = $_REQUEST['email'];
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email']; 
 $msg = $_REQUEST['message'];
 
 $headers = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
 $message = 'Name : ' . $name . "<br/>";
-$message .= 'Email : ' . $email . "<br/>";
+$message .= 'Email : ' . $email . "<br/>"; 
 $message .= $msg;
 
-if (@mail($emailTo, $subject, $message, $headers, "-r".$emailTo))
-{
-     // Transfer the value 'sent' to ajax function for showing success message.
-     echo 'sent';
-}
-else
-{
-     // Transfer the value 'failed' to ajax function for showing error message.
-     echo 'failed';
+if (@mail($emailTo, $subject, $message, $headers, "-r".$emailTo)) {
+    // Transfer the value 'sent' to ajax function for showing success message.
+    echo 'sent';
+} else {
+    // Transfer the value 'failed' to ajax function for showing error message.
+    echo 'failed';
 }
 ?>
